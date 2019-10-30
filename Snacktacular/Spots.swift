3c1ffc247dfa917c8f795416a2aa8a535a4e271f
Joseph Marasco <marascoj@bc.edu>
2019-10-30 19:48:10 -0400

    Initial Commit

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,14 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Joseph Marasco on 10/29/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
+
